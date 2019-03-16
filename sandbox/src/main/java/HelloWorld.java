3067a195c03623ceee90ae5f837b48d0b7fea346
lily.ibatulina@yandex.ru <007700gbc>
2019-03-16 16:47:35 +0400

    Create project for java trening

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -1,9 +1,11 @@
 class HelloWorld {
-
-public static void main (String args []) {
-
-System. out. println ("Hello World");
-
-}
+    public static void main (String args []) {
+        Point p1 = new Point(6, 5);
+        Point p2 = new Point(9, 3);
+        System.out.println(distance(p1, p2));
+    }
+    public static double distance (Point p1, Point p2){
+       return Math.sqrt ((p2.x -p1.x)*(p2.x -p1.x) - (p2.y -p1.y)*(p2.y -p1.y));
+    }
 
 }
